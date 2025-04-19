84ada10d7cee829b8a9ea255e0669d26f249112c
loughead123 <2782985733@qq.com>
2025-04-19 15:26:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
 function getWeather() {
     const city = document.getElementById('city').value;
-    const apiKey = 'd0f0a13a20ba47a1bbde5f534dd2868b'; 
-    const apiUrl = `https://devapi.qweather.com/v7/weather/now?location=${city}&key=${apiKey}`;
-
-    fetch(apiUrl)
+    const apiKey = 'cc9db6a4457354e1cd3df3c561e6c0eb';
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
         .then(response => response.json())
         .then(data => {
-            if (data.code === "200") {
-                document.getElementById('weather-info').innerHTML = `
-                    <h2>Temperature: ${data.now.temp}°C</h2>
-                    <h2>Weather: ${data.now.text}</h2>
-                    <h2>Humidity: ${data.now.humidity}%</h2>
-                    <h2>Wind Speed: ${data.now.windSpeed} km/h</h2>
-                `;
-            } else {
-                document.getElementById('weather-info').innerHTML = `<h2>${data.message}</h2>`;
-            }
+            console.log(data);
+            document.getElementById('weather-info').innerHTML = `
+                <h2>Temperature: ${data.main.temp}°C</h2>
+                <h2>Humidity: ${data.main.humidity}%</h2>
+                <h2>Wind Speed: ${data.wind.speed} km/h</h2>
+            `;
         })
-        .catch(error => {
-            console.error('Error:', error);
-            document.getElementById('weather-info').innerHTML = `<h2>Failed to fetch weather data.</h2>`;
-        });
+        .catch(error => console.error('Error:', error));
 }
